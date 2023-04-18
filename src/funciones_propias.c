e0d0bd0142fd48aefbd49f4478b57f4d2870b117
pamuca <129038796+pamuca@users.noreply.github.com>
2023-04-18 15:04:21 +0200

    Update funciones_propias.c
    Añadido generar_sopa y pequeños arreglos

--- src/funciones_propias.c
+++ src/funciones_propias.c
@@ -36,3 +36,76 @@ void mostrar_paraules ( struct Sopa_t * sopa ) {
         printf("%4s%-11s\n", "",sopa->paraules[i].contingut );
     }
 }
+
+void generar_sopa(struct Sopa_t *s)
+{
+	int aux, p_length;
+	int x, y, d;	// x i y son coordenadas de la sopa, d es dirección
+	bool correcto;
+	srand(time(NULL));
+
+    s->lletres = malloc(s->dim * s->dim * sizeof(char));   // Espai per a les lletres
+    s->encertades = malloc(s->dim * s->dim * sizeof(char)); // Per saber si una lletra correspon a encert
+    for (int i = 0; i < s->dim * s->dim; i++)
+    {
+        s->encertades[i] = false;
+        // Inicialitzem la taula amb guions
+        s->lletres[i] = '-';
+    }
+
+	for (int i = 0; i < s->n_paraules; i++)
+	{
+		p_length = strlen(s->paraules[i].contingut);
+		do
+		{
+			x = rand() % s->dim;		// Genera x aleatoria
+			y = rand() % s->dim;		// Genera y aleatoria
+			d = rand() % 2;				// Genera dirección aleatoria
+			if (d == 0)
+			{
+				correcto = (s->dim - x >= p_length);
+				aux = 0;
+				while(aux < p_length && correcto)
+				{
+					correcto = (s->lletres[y*s->dim+(x+aux)] == '-');		// i*N + j
+					aux++;
+				}
+			}
+			else
+			{
+				correcto = (s->dim - y >= p_length);
+				aux = 0;
+				while(aux < p_length && correcto)
+				{
+					correcto = (s->lletres[(y+aux)*s->dim+x] == '-');		// i*N + j
+					aux++;
+				}
+			}
+		}
+		while (!correcto);
+
+		if (d == 0)
+		{
+			for (int j = 0; j < p_length; j++)
+			{
+				s->lletres[y*s->dim + (x+j)] = s->paraules[i].contingut[j];
+			}
+		}
+		else
+		{
+			for (int j = 0; j < p_length; j++)
+			{
+				s->lletres[(y+j)*s->dim + x] = s->paraules[i].contingut[j];
+			}
+		}
+	}
+    for (int i = 0; i < s->dim * s->dim; i++)
+    {
+        s->encertades[i] = false;
+        // Generem una lletra aleatoriament
+		if (s->lletres[i] == '-')
+		{
+			s->lletres[i] = 'A' + (rand() % ('Z'-'A' + 1));
+		}
+    }
+}
